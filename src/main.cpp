77cdceef6d370ae7ba17d0ebe9824d7a0b991688
normalid <rluo2018@gmail.com>
2024-06-17 18:38:43 +0800

    done

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,10 @@
 #include "sdkconfig.h"
 #include "esp_sntp.h"
 #include <string>
+#include "Wire.h"
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps612.h>
+
 
 #include <ArduinoJson.h>
 #include <EEPROM.h>
@@ -50,7 +54,7 @@ Preferences stopplate_preferences_store;
 #define TIME_BETWEEN_HIT 0.05
 
 /* #region DEBUG TAG BLOCK */
-static const char *MIC_DEBUG_TAG = "Mic Sensor";
+static const char *SENSOR_DEBUG_TAG = "Sensor";
 static const char *BLE_NOTIFY_DEBUG_TAG = "BLE | Notify";
 static const char *LED_DEBUG_TAG = "LED";
 static const char *BLE_SERVER_DEBUG_TAG = "BLE | Server";
@@ -84,6 +88,7 @@ enum EStopplatePreferences
     BuzzerDuration,
     BuzzerFrequency,
     BuzzerWaveform,
+    Sensitive,
 };
 
 void load_preferences();
@@ -193,31 +198,26 @@ class timeSyncWriteCallbacks : public BLECharacteristicCallbacks
 {
     void onWrite(BLECharacteristic *pCharacteristic)
     {
-        try {
-            double time_received = std::stod(pCharacteristic->getValue());
-            double time_end = get_time();
-            double time_interval = time_end - time_init;
-            double time_final = (time_received) + ((time_interval) / 2) /* /2 divide by 2 is essential for cristian clock syncronize alg. but idk why it will decrease the accuracy. */;
-            set_time_double(time_final);
-            double error = std::fabs(get_time() - time_final);
-            ESP_LOGI(CLOCK_DEBUG_TAG, "time sync reply with: %s", std::to_string(time_received).c_str());
-            ESP_LOGI(CLOCK_DEBUG_TAG, "time_start: %s", std::to_string(time_init).c_str());
-            ESP_LOGI(CLOCK_DEBUG_TAG, "time_end: %s", std::to_string(time_end).c_str());
-            ESP_LOGI(CLOCK_DEBUG_TAG, "time_interval: %s", std::to_string(time_interval).c_str());
-            ESP_LOGI(CLOCK_DEBUG_TAG, "time_received: %s", std::to_string(time_received).c_str());
-            ESP_LOGI(CLOCK_DEBUG_TAG, "time_final: %s", std::to_string(time_final).c_str());
-            ESP_LOGI(CLOCK_DEBUG_TAG, "error: %s", std::to_string(error).c_str());
-            ESP_LOGI(BLE_GATT_DEBUG_TAG, "time sync write value raw: %s", pCharacteristic->getData());
-            ESP_LOGI(BLE_GATT_DEBUG_TAG, "time sync write value: %s", pCharacteristic->getValue().c_str());
-            if (error > 0.1 && clock_sync_retry_count < MAX_CLOCK_SYNC_RETRY_TIME)
-            {
-                ESP_LOGI(CLOCK_DEBUG_TAG, "retry count: %d", clock_sync_retry_count);
-                // retry until error < 0.1
-                notify_perform_clock_sync();
-            }
-        } catch (std::exception &e) {
-            ESP_LOGI(CLOCK_DEBUG_TAG, "time sync write value error: %s", e.what());
-            return;
+        double time_received = std::stod(pCharacteristic->getValue());
+        double time_end = get_time();
+        double time_interval = time_end - time_init;
+        double time_final = (time_received) + ((time_interval) / 2) /* /2 divide by 2 is essential for cristian clock syncronize alg. but idk why it will decrease the accuracy. */;
+        set_time_double(time_final);
+        double error = std::fabs(get_time() - time_final);
+        ESP_LOGI(CLOCK_DEBUG_TAG, "time sync reply with: %s", std::to_string(time_received).c_str());
+        ESP_LOGI(CLOCK_DEBUG_TAG, "time_start: %s", std::to_string(time_init).c_str());
+        ESP_LOGI(CLOCK_DEBUG_TAG, "time_end: %s", std::to_string(time_end).c_str());
+        ESP_LOGI(CLOCK_DEBUG_TAG, "time_interval: %s", std::to_string(time_interval).c_str());
+        ESP_LOGI(CLOCK_DEBUG_TAG, "time_received: %s", std::to_string(time_received).c_str());
+        ESP_LOGI(CLOCK_DEBUG_TAG, "time_final: %s", std::to_string(time_final).c_str());
+        ESP_LOGI(CLOCK_DEBUG_TAG, "error: %s", std::to_string(error).c_str());
+        ESP_LOGI(BLE_GATT_DEBUG_TAG, "time sync write value raw: %s", pCharacteristic->getData());
+        ESP_LOGI(BLE_GATT_DEBUG_TAG, "time sync write value: %s", pCharacteristic->getValue().c_str());
+        if (error > 0.1 && clock_sync_retry_count < MAX_CLOCK_SYNC_RETRY_TIME)
+        {
+            ESP_LOGI(CLOCK_DEBUG_TAG, "retry count: %d", clock_sync_retry_count);
+            // retry until error < 0.1
+            notify_perform_clock_sync();
         }
     }
 };
@@ -242,6 +242,7 @@ class settingCallbacks : public BLECharacteristicCallbacks
             int32_t buzzer_duration = json_doc["buzzer_duration"];
             int32_t buzzer_frequency = json_doc["buzzer_frequency"];
             int32_t buzzer_waveform = json_doc["buzzer_waveform"];
+            int32_t sensitive = json_doc["sensitive"];
 
             ESP_LOGI(JSON_DEBUG_TAG, "indicator_light_up_duration: %i,countdown_random_time_min: %f,countdown_random_time_max: %f,buzzer_duration: %i,buzzer_frequency: %i,buzzer_waveform: %i", indicator_light_up_duration, countdown_random_time_min, countdown_random_time_max, buzzer_duration, buzzer_frequency, buzzer_waveform);
             stopplate_preferences_store.begin("settings", false);
@@ -251,6 +252,7 @@ class settingCallbacks : public BLECharacteristicCallbacks
             stopplate_preferences_store.putInt(std::to_string(EStopplatePreferences::BuzzerDuration).c_str(), buzzer_duration);
             stopplate_preferences_store.putInt(std::to_string(EStopplatePreferences::BuzzerFrequency).c_str(), buzzer_frequency);
             stopplate_preferences_store.putInt(std::to_string(EStopplatePreferences::BuzzerWaveform).c_str(), buzzer_waveform);
+            stopplate_preferences_store.putInt(std::to_string(EStopplatePreferences::Sensitive).c_str(), sensitive);
             stopplate_preferences_store.end();
             load_preferences();
         }
@@ -413,6 +415,8 @@ void set_time_double(double sec)
     ESP_LOGI(CLOCK_DEBUG_TAG, "Set time: %f", sec);
 };
 
+int sensor_sensitive = 600;
+
 void load_preferences()
 {
     stopplate_preferences_store.begin("settings", false);
@@ -424,10 +428,98 @@ void load_preferences()
     json_doc_v["buzzer_duration"] = stopplate_preferences_store.getInt(std::to_string(EStopplatePreferences::BuzzerDuration).c_str());
     json_doc_v["buzzer_frequency"] = stopplate_preferences_store.getInt(std::to_string(EStopplatePreferences::BuzzerFrequency).c_str());
     json_doc_v["buzzer_waveform"] = stopplate_preferences_store.getInt(std::to_string(EStopplatePreferences::BuzzerWaveform).c_str());
+    sensor_sensitive = stopplate_preferences_store.getInt(std::to_string(EStopplatePreferences::Sensitive).c_str(), 600);
+    json_doc_v["sensitive"] = sensor_sensitive;
     serializeJson(json_doc_v, json_output_v);
     ESP_LOGI(JSON_DEBUG_TAG, "json_output: %s", json_output_v);
     settingCharacteristic->setValue(json_output_v);
     stopplate_preferences_store.end();
+}
+uint8_t devStatus;     // return status after each device operation (0 = success, !0 = error)
+bool dmpReady = false; // set true if DMP init was successful
+uint16_t packetSize;   // expected DMP packet size (default is 42 bytes)
+
+MPU6050 mpu;
+
+void initMPU()
+{
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+#endif
+
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    // while (Serial.available() && Serial.read())
+    //     ; // empty buffer
+    // while (!Serial.available())
+    //     ; // wait for data
+    // while (Serial.available() && Serial.read())
+    //     ; // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    // mpu.setXGyroOffset(220);
+    // mpu.setYGyroOffset(76);
+    // mpu.setZGyroOffset(-85);
+    // mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0)
+    {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        // Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        // Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        // Serial.println(F(")..."));
+        // attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        // mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+    else
+    {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
 }
 
 void esp_loop(void);
@@ -437,6 +529,8 @@ extern "C" void app_main()
     init_gpio_level();
 
     init_ble();
+
+    initMPU();
 
     set_time(1695827472, 100);
 
@@ -454,17 +548,45 @@ extern "C" void app_main()
 
 double previous_hit = 0;
 
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+VectorFloat gravity;    // [x, y, z]            gravity vector
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+Quaternion q;           // [w, x, y, z]         quaternion container
+unsigned long previousTime = 0;
+double previousRa = 0;
+double getFinalizedSensorValue()
+{
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
+    {
+        unsigned long times = millis();
+        float deltaTime = times - previousTime; // Calculate the time it takes to run a cycle
+        previousTime = times;
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        // ESP_LOGI(SENSOR_DEBUG_TAG, "ra.x:%i,ra.y:%i,ra.z:%i", aaReal.x, aaReal.y, aaReal.z);
+        double ra = sqrt(aaReal.x * aaReal.x + aaReal.y * aaReal.y + aaReal.z * aaReal.z);
+        double deltaRa = ra - previousRa;
+        previousRa = ra;
+        double raDirivative = deltaRa / deltaTime;
+        // ESP_LOGI(SENSOR_DEBUG_TAG, ",ra:%f,dra:%f", ra, deltaRa);
+        return raDirivative;
+    }
+    return 0;
+}
+
 void esp_loop(void)
 {
-    int analog_mic_val = adc1_get_raw(ANALOG_MIC);
-    int digital_mic_val = adc1_get_raw(DIGITAL_MIC);
-    if (analog_mic_val >= 1200)
+    double sensorValue = getFinalizedSensorValue();
+    if (sensorValue >= sensor_sensitive)
     {
         if (get_time() - previous_hit >= TIME_BETWEEN_HIT)
         {
 
-            ESP_LOGI(MIC_DEBUG_TAG, "Triggered");
-            ESP_LOGI(MIC_DEBUG_TAG, "analog_mic_val: %i, digital_mic_val: %i,", analog_mic_val, digital_mic_val);
+            ESP_LOGI(SENSOR_DEBUG_TAG, "Triggered");
+            ESP_LOGI(SENSOR_DEBUG_TAG, "With value: %f", sensorValue);
             toggle_led();
             notify_ble();
             previous_hit = get_time();
